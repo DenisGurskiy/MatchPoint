80bc3ec14faa4c12ad5236252644c4594fc9c70a
Denis Hurskiy <denis.gurskiy@gmail.com>
2024-06-14 10:23:56 +0200

    send email done

--- app/utils/email.utils.ts
+++ app/utils/email.utils.ts
@@ -3,10 +3,6 @@ import SMTPTransport from "nodemailer/lib/smtp-transport";
 
 require("dotenv").config();
 
-console.log("SMTP_HOST:", process.env.SMTP_HOST);
-console.log("SMTP_PORT:", process.env.SMTP_PORT);
-console.log("SMTP_USER:", process.env.SMTP_USER);
-
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: process.env.SMTP_PORT,
@@ -24,8 +20,6 @@ export const sendEmail = (email: string, password: string) => {
     <p style="font-size: 1.2em; font-weight: bold; color: #FF5722;">${password}</p>
     <p style="font-size: 0.9em; color: #999;">Please keep this password in a safe place.</p>
   </div>`;
-
-  console.log("send email...");
 
   return transporter.sendMail({
     from: "SportSpace Auth API",
