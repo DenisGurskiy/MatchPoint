9cfc1b6b39de2282f4c76b6b4713877f1f898120
Denis Hurskiy <denis.gurskiy@gmail.com>
2024-06-16 17:46:49 +0200

    suspense

--- app/grounds/page.tsx
+++ app/grounds/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 
+import { useEffect, useState } from "react";
+import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
+import Image from "next/image";
+
 import { GroundCardBlock } from "@/components/GroundCardBlock";
 import { SearchForm } from "@/components/SearchForm";
 import { SearchFormHeader } from "@/components/SearchFormHeader";
 import { ModalSearch } from "@/components/ui/modalSearch";
-import Image from "next/image";
-import { useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
 import { Ground } from "../types/ground";
 
 export default function Grounds() {
@@ -19,7 +21,8 @@ export default function Grounds() {
   const activity = searchParams.get("activity") || "";
   const date = searchParams.get("date");
 
-  const baseUrl = "https://sportspace.onrender.com/api/service/sports-complexes/";
+  const baseUrl =
+    "https://sportspace.onrender.com/api/service/sports-complexes/";
   const params = new URLSearchParams();
   params.append("location", location);
   params.append("activity", activity);
@@ -52,67 +55,69 @@ export default function Grounds() {
   }, [url]);
 
   return (
-    <div className="ownContainer flex flex-col gap-y-[24px] mt-[16px]">
-      <div className="hidden md:block">
-        <SearchForm />
-      </div>
-      <div className="md:hidden">
-        <div
-          id="searchFormMini"
-          className="w-full border-[1px] border-gray20divider h-[40px] rounded-[100px] px-[16px] flex items-center gap-[8px] cursor-pointer"
-          onClick={() => setIsSearchFormActive(true)}
-        >
-          <Image src="/images/Search.svg" alt="logo" width={24} height={24} />
-          <p>Kyiv · Tennis</p>
+    <Suspense>
+      <div className="ownContainer flex flex-col gap-y-[24px] mt-[16px]">
+        <div className="hidden md:block">
+          <SearchForm />
         </div>
-      </div>
-      {loading ? (
-        <div role="status" className="m-auto">
-          <svg
-            aria-hidden="true"
-            className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
-            viewBox="0 0 100 101"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
+        <div className="md:hidden">
+          <div
+            id="searchFormMini"
+            className="w-full border-[1px] border-gray20divider h-[40px] rounded-[100px] px-[16px] flex items-center gap-[8px] cursor-pointer"
+            onClick={() => setIsSearchFormActive(true)}
           >
-            <path
-              d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-              fill="currentColor"
-            />
-            <path
-              d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-              fill="currentFill"
-            />
-          </svg>
-          <span className="sr-only">Loading...</span>
+            <Image src="/images/Search.svg" alt="logo" width={24} height={24} />
+            <p>Kyiv · Tennis</p>
+          </div>
         </div>
-      ) : (
-        <>
-          {!!grounds.length ? (
-            <section className="ownGrid mb-[60px]">
-              {grounds.map((ground) => (
-                <GroundCardBlock key={ground?.id} image="new_3.png" />
-              ))}              
-              <GroundCardBlock image="new_2.png" />
-              <GroundCardBlock image="new_1.png" />
-              <GroundCardBlock image="new_4.png" />
-              <GroundCardBlock image="new_5.png" />
-              <GroundCardBlock image="new_4.png" />
-            </section>
-          ) : (
-            <p className="m-auto text-gray50">
-              There are no fields matching your criteria
-            </p>
-          )}
-        </>
-      )}
+        {loading ? (
+          <div role="status" className="m-auto">
+            <svg
+              aria-hidden="true"
+              className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+              viewBox="0 0 100 101"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                fill="currentColor"
+              />
+              <path
+                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                fill="currentFill"
+              />
+            </svg>
+            <span className="sr-only">Loading...</span>
+          </div>
+        ) : (
+          <>
+            {!!grounds.length ? (
+              <section className="ownGrid mb-[60px]">
+                {grounds.map((ground) => (
+                  <GroundCardBlock key={ground?.id} image="new_3.png" />
+                ))}
+                <GroundCardBlock image="new_2.png" />
+                <GroundCardBlock image="new_1.png" />
+                <GroundCardBlock image="new_4.png" />
+                <GroundCardBlock image="new_5.png" />
+                <GroundCardBlock image="new_4.png" />
+              </section>
+            ) : (
+              <p className="m-auto text-gray50">
+                There are no fields matching your criteria
+              </p>
+            )}
+          </>
+        )}
 
-      <ModalSearch
-        isActive={isSearchFormActive}
-        setIsActive={setIsSearchFormActive}
-      >
-        <SearchFormHeader setIsActive={setIsSearchFormActive} />
-      </ModalSearch>
-    </div>
+        <ModalSearch
+          isActive={isSearchFormActive}
+          setIsActive={setIsSearchFormActive}
+        >
+          <SearchFormHeader setIsActive={setIsSearchFormActive} />
+        </ModalSearch>
+      </div>
+    </Suspense>
   );
 }
